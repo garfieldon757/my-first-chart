3cf3afa8db0f06bd5c783113b8ad13bb78a3fc02
oliverfan <oliverfan@tencent.com>
2021-08-05 14:45:41 +0800

    add: lineChart类

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import LineChart from './Chart.js';
+
+function init() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  window.ctx = ctx;
+}
+
+function setOption(option) {
+  const ctx = window.ctx;
+
+  const lineChart = new LineChart(ctx, option);
+  lineChart.render();
+}
+
 
 function App() {
+
+  useEffect(() => {
+    // 1.初始化画布
+    init();
+    // 2.首次绘制
+    const option = {
+      data: [50,45,46,42,40,42,35],
+      xAxis: [10,11,12,13,14, 15,16],
+      yAxis: [30,40,50,60]
+    };
+    setOption(option);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <canvas 
+        id="canvas"
+        width="600" 
+        height="300"
+      ></canvas>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +53,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,124 @@
+export default class LineChart {
+    constructor(context, option) {
+      this.ctx = context;
+      this.option = option;
+
+      const {
+        originX, originY
+      } = this._calOriginCoord();
+      this.originX = originX;
+      this.originY = originY;
+    }
+    render() {
+        const ctx = this.ctx;
+    
+        // 1.画布轮廓
+        ctx.strokeRect(0, 0, 600, 300);
+        // 2.坐标轴
+        ctx.beginPath();
+        this._drawAxis({
+            ctx: this.ctx,
+            originCoord: {
+                originX: this.originX,
+                originY: this.originY
+            },
+            data: this.option.data
+        });
+        // 3.折线
+        this._drawLineDataPoints({ 
+            data: this.option.data, 
+            ctx: this.ctx 
+        });
+        ctx.stroke();
+    }
+
+    // 计算坐标轴原点坐标
+    _calOriginCoord() {
+      return {
+        originX: 50, 
+        originY: 250
+      };
+    }
+
+    // 转换数值到坐标点
+    _convertVal2Coord(val, index) {
+        const scaleFactor = 5;
+        return {
+            x: 50  + 50 * ( index + 1 ),
+            y: 100 + (50 - val) * scaleFactor, 
+        };
+    }
+
+    // 坐标轴绘制
+    _drawAxis({ ctx, originCoord, data }) {
+        this._doDrawAxis({
+            type: 'x',
+            axisData: this.option.xAxis,
+            data,
+            ctx,
+            originCoord,
+        });
+        this._doDrawAxis({
+            type: 'y',
+            axisData: this.option.yAxis,
+            ctx,
+            originCoord
+        });
+    }
+
+    _doDrawAxis({ type, axisData, data, ctx, originCoord }) {
+        const { originX, originY } = originCoord;
+        switch(type) {
+            case 'x':
+            {
+                ctx.moveTo(originX, originY);
+                let xAxisXCoord, xAxisYCoord;
+                axisData.forEach((val, index) => {
+                    xAxisXCoord = 50 + 50 * (index+1);
+                    xAxisYCoord = 250;
+                    ctx.lineTo(xAxisXCoord, xAxisYCoord);
+                    ctx.arc(xAxisXCoord, xAxisYCoord, 2, 0, 2 * Math.PI);
+                });
+                // 延续坐标轴
+                ctx.lineTo(xAxisXCoord + 50, xAxisYCoord);
+                break;
+            }
+            case 'y':
+            {
+                ctx.moveTo(originX, originY);
+                let yAxisXCoord = 50, yAxisYCoord = 250;
+
+                // TODO: 自动计算Y轴范围
+                // const { minY, maxY } = this._calYCoordRange(data);
+
+                axisData.forEach((val, index) => {
+                    yAxisYCoord = 250 - 50 * index;
+                    ctx.lineTo(yAxisXCoord, yAxisYCoord);
+                    ctx.arc(yAxisXCoord, yAxisYCoord, 2, 0, 2 * Math.PI);
+                    ctx.moveTo(yAxisXCoord, yAxisYCoord); // fix: 竖直方向上画完点后画笔会偏移,手动修正
+                });
+                ctx.lineTo(yAxisXCoord, yAxisYCoord - 50); // 延续坐标轴
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+
+    // 绘制折线
+    _drawLineDataPoints({ data, ctx }) {
+        let drawFlag = false;
+        data.forEach((val, index) => {
+            let { x, y } = this._convertVal2Coord(val, index);
+            if(!drawFlag){
+            drawFlag = true;
+            ctx.moveTo(x,y);
+            } else {
+            ctx.lineTo(x,y);
+            }
+            ctx.arc(x, y, 2, 0, 2 * Math.PI);
+        });
+    }
+
+  }
